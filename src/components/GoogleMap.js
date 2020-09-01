dbb80a3cbbb1b3cedda123aabe4c31130996f6f7
JFizzle-git <55044585+JFizzle-git@users.noreply.github.com>
2020-09-01 22:49:24 +1000

    Adding contact details and removing contact form as it needs setting up

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -10,8 +10,8 @@ if (process.env.NETLIFY_MAP_KEY) {
 class GoogleMap extends Component {
   static defaultProps = {
     center: {
-      lat: -28.0914483,
-      lng: 153.4425208
+      lat: -33.636277,
+      lng: 151.329490
     },
     zoom: 14
   }
@@ -25,8 +25,8 @@ class GoogleMap extends Component {
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
-          <Marker lat={-28.0914483} lng={153.4425208} text={'Kreyser Avrora'} />
-        </GoogleMapReact>
+          <Marker lat={-33.636277} lng={151.329490} text={'Lamont Dance Theatre'} />
+        </GoogleMapReact> 
       </div>
     )
   }
